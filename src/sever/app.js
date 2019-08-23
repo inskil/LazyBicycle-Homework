e3ad97a243b3c6cfb302719fffb1a38dbb4ffb07
inskil <pj0713@126.com>
2019-08-23 23:27:57 +0800

    update config

--- src/sever/app.js
+++ src/sever/app.js
@@ -8,6 +8,8 @@ import './mongodb'
 const app = new Koa()
 middleware(app)
 router(app)
+
+
 app.listen(conf.port, '0.0.0.0', () => {
     console.log(`server is running at http://${ip.address()}:${conf.port}`)
 })
