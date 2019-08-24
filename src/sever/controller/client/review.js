8330c3a270c9b54bbb018db065ecd99c07041c81
inskil <pj0713@126.com>
2019-08-24 16:47:57 +0800

    fix review bug

--- src/sever/controller/client/review.js
+++ src/sever/controller/client/review.js
@@ -7,7 +7,7 @@ module.exports = {
         console.log('----------------按照所给数量分配所给id和条数的评论,0则all-----------------------');
         let { id="" ,start=1,count=0 } = ctx.request.query;
         try {
-            let data = await ctx.find(bookreviewModel,{"id" : {$regex:id}},null,{limit: count, skip: (start-1)*count, sort: {level: -1, createTime: -1}});
+            let data = await ctx.find(bookreviewModel,{"start" : id},null,{limit: count, skip: (start-1)*count, sort: {level: -1, createTime: -1}});
             return ctx.send(data);
         } catch (e) {
             return ctx.sendError(e)
@@ -17,24 +17,14 @@ module.exports = {
         console.log('----------------按照所给数量分配所给id和条数的评论,0则all-----------------------');
         let { id="" ,start=1,count=0 } = ctx.request.query;
         try {
-            let data = await ctx.find(reviewModel,{"id" : {$regex:id}},null,{limit: count, skip: (start-1)*count, sort: {level: -1, createTime: -1}});
+            let data = await ctx.find(reviewModel,{"start" :id},null,{limit: count, skip: (start-1)*count, sort: {level: -1, createTime: -1}});
             console.log(data)
             return ctx.send(data)
         }catch (e){
             console.log(e)
             return ctx.sendError(e)
         }
-    },
+    }
 
-    async OneInfoByKey(ctx, next) {
-        console.log('----------------获取指定一条指定titleid的info-----------------------');
-        let { title="" } = ctx.request.query;
-        try{
-            let data = await ctx.findOne(bookModel,{"title" : {$regex:title}});
-            return ctx.send(data);
-        } catch (e) {
-            return ctx.sendError(e)
-        }
-    },
 
 }
