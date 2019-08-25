c1c3cebe54d930bc4d6e9ed9e26bac0792f4adba
linan1109 <linan119@yeah.net>
2019-08-25 11:56:48 +0800

    finishl

--- src/components/detailinfo.vue
+++ src/components/detailinfo.vue
@@ -6,7 +6,7 @@
                 <div class="clear"></div>
             </h1>
         </div>
-        <div class="main_body">
+        <div class="main_body; width: 100%">
             <div class="infomations">
                 <div id="mainpic">
                     <a class="nbg" v-bind:href="bigimg"
@@ -30,7 +30,7 @@
                     <span class="rl">评分:<br> <strong>{{average}}</strong></span><br>
                 </div>
                 <div>
-                    <Rate disabled allow-half v-model="stars"/>
+                    <Rate disabled allow-half v-model="stars" />
                     <br>
                 </div>
                 <div>
@@ -40,7 +40,18 @@
             <div class="related_info">
             </div>
         </div>
-        <div style="height: 50px"></div>
+        <div style="height: 50px; width: 100%"></div>
+        <div style="height: 70px;width: 100%;">
+            <div style="float: left;width: 15%;height: 100%">
+                你的评价：
+                <Rate :value.sync="value"></Rate>
+            </div>
+            <div style="float: right;width: 30%;height: 100%">
+                <i-button @click="heart_success(true)" icon="ios-heart">收藏</i-button>
+            </div>
+        </div>
+
+
         <div class="summary_body">
             <div id="summary_title">
                 <h1 style="text-align: left">
@@ -72,18 +83,10 @@
                     <div class="clear"></div>
                 </h1>
             </div>
-            <div :class="showTotal ? 'total-introduce' : 'detailed-introduce'">
-                <div class="intro-content " ref="desc">
-                    <div class="merchant-desc" style="text-align: left" v-if="introduce">
-                        <div id="catalog_summary">
-                            <pre class="p3" style="text-align: left">{{catalog}}</pre>
-                        </div>
-                    </div>
-                    <div class="unfold" @click="showTotalIntro" v-if="showExchangeButton">
-                        <p>{{exchangeButton ? '展开' : '收起'}}</p>
-                    </div>
-                </div>
+            <div id="catalog_summary">
+                <pre class="p3" style="text-align: left">{{catalog}}</pre>
             </div>
+
         </div>
         <div style="height: 50px"></div>
         <div id="review_body">
@@ -95,7 +98,7 @@
             </div>
             <div style="height: 20px; width: 100%"></div>
             <div id="review_review">
-                <div v-for="review in reviews" id="review_author">
+                <div v-for="review in reviews" v-bind:key="review.id" id="review_author">
                     <div style="width: 70%;float: left;height: 25px">
                         <p class="p4" style="text-align: left; color: #666666">{{review.author.name}} ·
                             {{review.published}} </p>
@@ -113,25 +116,37 @@
                 </div>
                 <div style="height: 15px; width: 100%"></div>
             </div>
+            <div id="review_new" style="width: 100%;">
+                <div id="review_kuang" style="width: 79%; float:left">
+                <i-input type="textarea" :autosize="{minRows: 3,maxRows: 5}" placeholder="请输入..."></i-input>
+                </div>
+                <div id="review_buttom" style="width: 20%;float: right">
+                    <div style="float:top;height:40px" ></div>
+                    <div style="float:left;">
+                        <i-button @click="review_success(true)">发表评论</i-button>
+                    </div>
+                </div>
+            </div>
+
         </div>
-        <div style="height: 50px"></div>
+        <div style="height: 200px"></div>
+        <Back-top></Back-top>
     </div>
+
 </template>
 
 <script>
+
+
     export default {
+
         name: "detailinfo",
         data() {
             return {
-                title: '演示展开收起',
-                introduce: '',
-                // 是否展示所有文本内容
-                showTotal: true,
-                // 显示展开还是收起
-                exchangeButton: true,
-                // 是否显示展开收起按钮
-                showExchangeButton: false,
-                rem: '',
+
+                value: 0,
+
+
                 id: 1234567,
                 title: 'balala',
                 bigimg: 'https://img3.doubanio.com/view/subject/l/public/s33446751.jpg',
@@ -187,232 +202,26 @@
                 ]
             }
         },
-        mounted() {
-            this.init();
-        },
         methods: {
-            showTotalIntro() {
-                console.log(this.showTotal);
-                this.showTotal = !this.showTotal;
-                this.exchangeButton = !this.exchangeButton;
-            }
-            ,
-            getRem() {
-                console.log('getRem');
-                const defaultRem = 16;
-                let winWidth = window.innerWidth;
-                console.log('winWidth:' + winWidth);
-                let rem = winWidth / 375 * defaultRem;
-                return rem;
-            }
-            ,
-            init() {
-                this.introduce = false;
-            }
-            ,
-            tryLong() {
-                let longIntroduce = this.catalog;
-                if (this.introduce !== longIntroduce) {
-                    this.showExchangeButton = false;
-                    this.showTotal = true;
-                    this.introduce = longIntroduce;
-                }
-            }
-            ,
-            tryShort() {
-                let shortIntroduce = this.catalog;
-                if (this.introduce !== shortIntroduce) {
-                    this.showExchangeButton = false;
-                    this.showTotal = true;
-                    this.introduce = shortIntroduce;
-                }
-            }
-        },
-        watch: {
-            'introduce': function () {
-                this.$nextTick(function () {
-                    console.log('nextTick');
-// 判断介绍是否超过四行
-                    let rem = parseFloat(this.getRem());
-                    console.log('watch 中的rem', rem);
-                    if (!this.$refs.desc) {
-                        console.log('desc null');
-                        return;
-                    }
-                    let descHeight = window.getComputedStyle(this.$refs.desc).height.replace('px', '');
-                    console.log('descHeight:' + descHeight);
-                    console.log('如果 descHeight 超过' + (5.25 * rem) + '就要显示展开按钮');
-                    if (descHeight > 5.25 * rem) {
-                        console.log('超过了四行');
-// 显示展开收起按钮
-                        this.showExchangeButton = true;
-                        this.exchangeButton = true;
-// 不是显示所有
-                        this.showTotal = false;
-                    } else {
-// 不显示展开收起按钮
-                        this.showExchangeButton = false;
-// 没有超过四行就显示所有
-                        this.showTotal = true;
-                        console.log('showExchangeButton', this.showExchangeButton);
-                        console.log('showTotal', this.showTotal);
-                    }
-                }.bind(this));
-            }
+            review_success(nodesc) {
+                this.$Notice.success({
+                    title: '评价发表成功',
+                    desc: nodesc ? '' : ''
+                });
+            },
+            heart_success(nodesc) {
+                this.$Notice.success({
+                    title: '收藏成功',
+                    desc: nodesc ? '' : ''
+                });
+            },
         }
     }
-    ;
+
+
 </script>
 
-<style lang="less" scoped rel="stylesheet/less">
+<style scoped>
     @import "../assets/css/infos.css";
 
-    .p3 {
-        font: 14px Arial;
-        line-height: 150%;
-        height: 440px;
-        overflow: hidden;
-        text-overflow: ellipsis;
-    }
-
-    .top-prove {
-        height: 100px;
-        width: 100%;
-        background: #dddddd;
-        text-align: center;
-        line-height: 100px;
-    }
-
-    .total-introduce {
-        height: auto;
-        overflow: hidden;
-        font-size: 14px;
-        color: #434343;
-        margin: 10px;
-
-        .intro-content {
-            .merchant-desc {
-                width: 100%;
-                line-height: 21px;
-            }
-        }
-
-        .unfold {
-            display: block;
-            z-index: 11;
-            float: right;
-            width: 40px;
-            height: 21px;
-
-            p {
-                margin: 0;
-                line-height: 21px;
-                color: #7fbe87;
-            }
-        }
-    }
-
-    .detailed-introduce {
-        font-size: 14px;
-        color: #434343;
-        position: relative;
-        overflow: hidden;
-        margin: 10px;
-
-        .intro-content {
-            // 最大高度设为4倍的行间距
-            max-height: 84px;
-            line-height: 21px;
-            word-wrap: break-word;
-            /*强制打散字符*/
-            word-break: break-all;
-            background: #ffffff;
-            /*同背景色*/
-            color: #ffffff;
-            overflow: hidden;
-
-            .merchant-desc {
-                width: 100%;
-                line-height: 21px;
-            }
-
-            &:after,
-                // 这是展开前实际显示的内容
-            &:before {
-                content: attr(title);
-                position: absolute;
-                left: 0;
-                top: 0;
-                width: 100%;
-                color: #434343
-                // overflow: hidden;
-            }
-
-            // 把最后最后一行自身的上面三行遮住
-            &:before {
-                display: block;
-                overflow: hidden;
-                z-index: 1;
-                max-height: 63px;
-                background: #ffffff;
-            }
-
-            &:after {
-                display: -webkit-box;
-                -webkit-box-orient: vertical;
-                overflow: hidden;
-                height: 81px;
-                /*截取行数*/
-                -webkit-line-clamp: 4;
-                text-overflow: ellipsis;
-                -webkit-box-sizing: border-box;
-                box-sizing: border-box;
-                /*行首缩进字符数，值为[(截取行数-1)*尾部留空字符数]*/
-                text-indent: -12em;
-                /*尾部留空字符数*/
-                padding-right: 4em;
-            }
-
-            .unfold {
-                z-index: 11;
-                width: 40px;
-                height: 21px;
-                outline: 0;
-                position: absolute;
-                right: 0;
-                bottom: 0;
-
-                p {
-                    margin: 0;
-                    line-height: 21px;
-                    color: #7fbe87;
-                }
-            }
-        }
-    }
-
-    .bottom-prove {
-        height: 100px;
-        width: 100%;
-        background: #dddddd;
-        text-align: center;
-        line-height: 100px;
-    }
-
-    .change-buttom {
-        font-size: 14px;
-        color: #2371be;
-
-        .long {
-            text-align: 21px;
-            text-align: center;
-            height: 21px;
-        }
-
-        .short {
-            text-align: 21px;
-            text-align: center;
-            height: 20px;
-        }
-    }
 </style>
