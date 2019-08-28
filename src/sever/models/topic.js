1276ce3d3aa34eb51cc5ee107dd6ea98c8111c59
linan1109 <linan119@yeah.net>
2019-08-28 09:31:28 +0800

    change models topic

--- src/sever/models/topic.js
+++ src/sever/models/topic.js
@@ -1,3 +1,15 @@
 import db from '../mongodb'
-let topicSchema = db.Schema([],{collection:"topic"})
+
+let topicSchema = db.Schema({
+    tid: String,
+    createtime: String,
+    title: String,
+    text: String,
+    gid: String,
+    uid: String,
+    username: String,
+    userheaedimg: String,
+    review: Array,
+},{collection:"topic"})
 export default db.model('topic', topicSchema)
+
